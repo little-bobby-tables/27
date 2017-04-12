8b6a2fbbff621ff68573ab5f33b1ddec5f60b12b
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-04-12 15:24:26 +0700

    oops

--- 3105.hs
+++ 3105.hs
@@ -4,5 +4,7 @@ main :: IO ()
 main = do
     cs <- getLine
     let ds = [ d | d <- takeWhile (/= '.') cs, d `elem` ['0'..'9'] ]
-    putStrLn $ reverse $ sort ds
+    if length ds == 0
+       then putStrLn "-1"
+       else putStrLn $ reverse $ sort ds
 
